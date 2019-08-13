9d27d6d6896b71fee10bc178774a68bb61c13bf0
Andres Alba <bryanalba92@gmail.com>
2019-08-12 22:52:55 -0500

    Autenticación con Google

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -3,6 +3,9 @@ import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/fire
 import { Mensaje } from '../interface/mensaje.interface';
 import { map } from 'rxjs/operators';
 
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
+
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +13,36 @@ import { map } from 'rxjs/operators';
 export class ChatService {
 
   public chats: Mensaje[] = [];
+  public usuario: any = {};
+
   private itemsCollection: AngularFirestoreCollection<Mensaje>;
 
-  constructor(private afs: AngularFirestore) {
+  constructor(private afs: AngularFirestore, public afAuth: AngularFireAuth) {
+
+    // authState es el estado de la autenticacion, de esta manera se va a estar escuchando
+    // cualquier cambio que sucesa en el estado de la autenticacion
+    this.afAuth.authState.subscribe( user => {
+      console.log('Estado del usuario: ', user );
+
+      if (!user ) {
+        return;
+      }
+
+      this.usuario.name = user.displayName;
+      this.usuario.uid = user.uid;
+
+    });
    }
 
+
+   login( proveedor: string) {
+    this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
+  }
+
+
+  logout() {
+    this.afAuth.auth.signOut();
+  }
    cargarMensajes() {
     this.itemsCollection = this.afs.collection<Mensaje>('chats', ref => ref.orderBy('fecha', 'desc').limit(5));
     return this.itemsCollection.valueChanges().pipe(map((mensajes: Mensaje[]) => {
